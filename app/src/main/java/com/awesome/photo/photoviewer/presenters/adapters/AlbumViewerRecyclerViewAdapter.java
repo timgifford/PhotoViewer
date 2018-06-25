71ac7fb97fb8e64321d2fa4077e1cd6bf2699508
Ross Sickora <katesickora@Kates-MacBook-Pro.local>
2018-06-24 22:39:02 -0500

    adding the recyclerview

--- app/src/main/java/com/awesome/photo/photoviewer/presenters/adapters/AlbumViewerRecyclerViewAdapter.java
+++ app/src/main/java/com/awesome/photo/photoviewer/presenters/adapters/AlbumViewerRecyclerViewAdapter.java
@@ -2,9 +2,12 @@ package com.awesome.photo.photoviewer.presenters.adapters;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.awesome.photo.photoviewer.R;
 import com.awesome.photo.photoviewer.presenters.Album;
 
 import java.util.List;
@@ -13,15 +16,24 @@ public class AlbumViewerRecyclerViewAdapter extends RecyclerView.Adapter {
 
     List<Album> albums;
 
+    public AlbumViewerRecyclerViewAdapter(List<Album> albums) {
+        this.albums = albums;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view_album_holder, viewGroup, false);
+
+        RecyclerView.ViewHolder holder = new AlbumViewerRecyclerViewAdapter.AlbumViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        Album current = albums.get(i);
 
+        ((AlbumViewerRecyclerViewAdapter.AlbumViewHolder) viewHolder).getAlbumTitleTextView().setText("Album " + current.getId());
     }
 
     @Override
@@ -31,9 +43,15 @@ public class AlbumViewerRecyclerViewAdapter extends RecyclerView.Adapter {
 
     public class AlbumViewHolder extends RecyclerView.ViewHolder {
 
+        private TextView albumTitleTextView;
 
         public AlbumViewHolder(@NonNull View itemView) {
             super(itemView);
+            albumTitleTextView = itemView.findViewById(R.id.album_title);
+        }
+
+        public TextView getAlbumTitleTextView() {
+            return albumTitleTextView;
         }
     }
 }
